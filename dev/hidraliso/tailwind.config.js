8f839a29acd7ca2731cebd6cc1876f30ca431caf
ricardojosino <ricardojosino@gmail.com>
2022-10-04 16:10:43 -0300

    hidraliso

--- dev/hidraliso/tailwind.config.js
+++ dev/hidraliso/tailwind.config.js
@@ -12,9 +12,9 @@ module.exports = {
 
       colors : {
         'color01' : '#1A1A1A',
-        'color02' : '#034D5A',
-        'color03' : '#CBA340',
-        'color04' : '#EDE6A4',
+        'color02' : '#8F2FAC',
+        'color03' : '#F366D7',
+        'color04' : '#25D366',
         'color05' : '#D8DBEE',
         'color06' : '#EEEEEE',
         'color07' : '#FFFFFF',
